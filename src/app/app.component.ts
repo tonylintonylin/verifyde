d8e27b55dc1bcd9b7274bae39d445184ef8babd8
Tony Lin <tonylinbusiness@gmail.com>
2021-01-12 16:11:01 -0500

    added sleep intervals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'verifyed';
+export class AppComponent implements AfterViewInit {
+  @ViewChild("verifydeWrapper") wrapper: ElementRef;
+  
+  ngAfterViewInit() {
+    console.log("naafterinit");
+
+    setInterval(() => { this.appear(); }, 2000);
+    setInterval(() => { this.disaappear(); }, 3000);
+  }
+
+  appear() {
+    console.log("appear");
+    this.wrapper.nativeElement.style.opacity = 1;
+  }
+
+  disaappear() {
+    console.log("disaappear");
+    this.wrapper.nativeElement.style.opacity = 0;
+  }
 }
